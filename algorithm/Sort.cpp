3a2c9c7d8d5fa8f3f3e8629da223ac64493f6a3f
gidskql6671 <gidskql6671@gmail.com>
2021-03-14 18:12:17 +0900

    selection Sort 구현

--- algorithm/Sort.cpp
+++ algorithm/Sort.cpp
@@ -7,6 +7,8 @@ bool defaultCompare(T a, T b);
 template <typename T> 
 void insertionSort(T arr[], int n, bool (*compare)(T, T) = defaultCompare);
 
+template <typename T> 
+void selectionSort(T arr[], int n, bool (*compare)(T, T) = defaultCompare);
 
 
 
@@ -20,7 +22,7 @@ bool defaultCompare(T a, T b){
 // Insertion Sort의 구현
 // T arr[] : 정렬할 1차원 배열
 // int n : 배열 안 원소의 개수
-// bool (*compare)(T, T) : 원소를 비교해주는 정렬 함수 
+// bool (*compare)(T, T) : 원소를 비교해주는 비교 함수 
 template <typename T> 
 void insertionSort(T arr[], int n, bool (*compare)(T, T)){
     // arr[0] ~ arr[0] 까지는 원소가 한개이므로 당연히 정렬된 상태. 즉 인덱스 1부터 시작
@@ -34,5 +36,26 @@ void insertionSort(T arr[], int n, bool (*compare)(T, T)){
             arr[j+1] = arr[j];
         
         arr[j+1] = key; // key보다 작은 값을 가지는 원소 뒤에 key를 넣어줌.
+    }
+}
+
+// Selection Sort의 구현
+// T arr[] : 정렬할 1차원 배열
+// int n : 배열 안 원소의 개수
+// bool (*compare)(T, T) : 원소를 비교해주는 비교 함수 
+template <typename T> 
+void selectionSort(T arr[], int n, bool (*compare)(T, T)){
+    for(int i = 0; i < n; i++){
+        int minIdx = i;
+        
+        for(int j = i + 1; j < n; j++)
+            if (compare(arr[j], arr[minIdx]))
+                minIdx = j;
+        
+        if (i != minIdx){
+            T tmp = arr[i];
+            arr[i] = arr[minIdx];
+            arr[minIdx] = tmp;
+        }
     }
 }
