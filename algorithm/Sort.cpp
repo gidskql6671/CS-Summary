2b3fe846c6e98cbf8e57eb89ddf6810b98ab82eb
gidskql6671 <gidskql6671@gmail.com>
2021-03-15 20:11:52 +0900

    Quick Sort 구현 및 SWAP문 매크로 함수화

--- algorithm/Sort.cpp
+++ algorithm/Sort.cpp
@@ -1,4 +1,5 @@
 /* Sorting Algorithm을 구현 */
+#define SWAP(x, y, tmp) ( (tmp) = (x), (x)=(y), (y)=(tmp) )
 
 template <typename T>
 bool defaultCompare(T a, T b);
@@ -13,6 +14,8 @@ void selectionSort(T arr[], int n, bool (*compare)(T, T) = defaultCompare);
 template <typename T> 
 void bubbleSort(T arr[], int n, bool (*compare)(T, T) = defaultCompare);
 
+template<typename T>
+void quickSort(T arr[], int left, int right, bool (*compare)(T, T) = defaultCompare);
 
 
 // 기본 비교 함수
@@ -62,9 +65,8 @@ void selectionSort(T arr[], int n, bool (*compare)(T, T)){
         
         // 선택된 값이 i번째 인덱스에 있지 않다면 이동해준다.
         if (i != target){
-            T tmp = arr[i];
-            arr[i] = arr[target];
-            arr[target] = tmp;
+            T tmp;
+            SWAP(arr[i], arr[target], tmp);
         }
     }
 }
@@ -81,11 +83,60 @@ void bubbleSort(T arr[], int n, bool (*compare)(T, T)){
         for(int j = 0; j < i; j++){
             // 인접한 두 원소를 비교해가며, 정렬되어있지 않다면 뒤로 민다.
             if (compare(arr[j + 1], arr[j])){
-                T tmp = arr[j];
-                arr[j] = arr[j+1];
-                arr[j+1] = tmp;
+                T tmp;
+                SWAP(arr[j], arr[j+1], tmp);
             }
         }
     }
 }
 
+// Quick Sort의 구현
+// 오름차순 정렬을 기준으로 설명
+// 1. pivot을 기준으로 2개의 부분 배열로 분할한다.
+// 2. pivot보다 작은 값은 왼쪽으로, pivot보다 큰 값을 오른쪽으로 옮긴다.
+// T arr[] : 정렬할 1차원 배열
+// int left : 정렬할 배열의 첫번째 인덱스
+// int right : 정렬할 배열의 마지막 인덱스
+// bool (*compare)(T, T) : 원소를 비교해주는 비교 함수 
+template<typename T>
+void quickSort(T arr[], int left, int right, bool (*compare)(T, T)){
+    
+    // 정렬할 배열의 크기가 2이상일 경우 수행.
+    // left == right 일 경우, 원소가 하나이므로 항상 정렬된 상태
+    if (left < right){
+        T tmp; // SWAP을 위한 임시 변수
+        int pivot = left; // pivot은 가장 왼쪽 값으로 결정.
+        int low = left;
+        int high = right + 1;
+        
+        
+        do{
+            // low가 배열의 범위를 벗어나거나
+            // arr[low]가 arr[pivot]보다 크거나 같은 경우 탈출.
+            do {
+                low++;
+            } while(low <= right && arr[low] < arr[pivot]);
+            
+            // high가 배열의 범위를 벗어나거나
+            // arr[high]가 arr[pivot]보다 작거나 같은 경우 탈출.
+            do {
+                high--;
+            } while(high >= left && arr[high] > arr[pivot]);
+            
+            // 위의 두개의 반복문을 통해 
+            // arr[low]에는 pivot보다 큰 값이 arr[high]에는 pivot보다 작은 값이 들어있다.
+            // 만약 low < high인 경우 정렬이 되어있지 않다는 것이므로 서로 바꿔줌.
+            if (low < high){
+                SWAP(arr[low], arr[high], tmp);
+            }
+        } while(low < high);
+        
+        // pivot 값을 기준으로 왼쪽에 작은 값들을 오른쪽에 큰값들을 몰기위한 SWAP
+        SWAP(arr[pivot], arr[high], tmp);
+        
+        // pivot을 기준으로 왼쪽에는 작은 값들, 오른쪽에는 큰 값들이 위치하도록 정렬됨.
+        // 이 두 배열에 대해서도 정렬을 시작함.
+        quickSort(arr, left, high - 1);
+        quickSort(arr, high + 1, right);
+    }
+}
